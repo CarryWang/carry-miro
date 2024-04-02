30480c8135a1212b3099b2987356b5e9ce030bbd
CarryWang <design.carrywang@gmail.com>
2024-04-02 10:47:22 +0800

    Fix search and favorite filter

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -4,22 +4,19 @@ import { useOrganization } from "@clerk/nextjs";
 
 import { EmptyOrg } from "./_components/empty-org";
 import { BoardList } from "./_components/board-list";
+import { useSearchParams } from "next/navigation";
 
-interface DashboardPageProps {
-  searchParams: {
-    search?: string;
-    favorites?: string;
-  };
-}
-
-const DashboardPage = ({ searchParams }: DashboardPageProps) => {
+const DashboardPage = () => {
   const { organization } = useOrganization();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || undefined;
+  const favorites = searchParams.get("favorites") || undefined;
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
       {!organization ? (
         <EmptyOrg />
       ) : (
-        <BoardList orgId={organization.id} query={searchParams} />
+        <BoardList orgId={organization.id} query={{ search, favorites }} />
       )}
     </div>
   );
